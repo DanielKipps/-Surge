00f26a99bdb7b732cfdac8dde84674b0ed5f9ca1
DanielKipps <77824387+dhawudh@users.noreply.github.com>
2022-06-05 00:45:52 +0800

    Update ipcheck.js

--- ipcheck.js
+++ ipcheck.js
@@ -9,7 +9,7 @@ $httpClient.get(url, function(error, response, data){
     let ip = jsonData.query
   body = {
     title: "IP Check",
-    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
+    content: `IP：${ip}\nOperator：${isp}\nArea：${emoji}${country} - ${city}`,
     icon: "globe.asia.australia.fill"
   }
   $done(body);
