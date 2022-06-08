6c528a4618015343ae3cbfb68dabd25c05c228ab
DanielKipps <77824387+dhawudh@users.noreply.github.com>
2022-06-09 02:55:31 +0800

    Update Streaming_media.js

--- Streaming_media.js
+++ Streaming_media.js
@@ -22,6 +22,8 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: 'Stream media',
       content: '',
+       icon: 'swift',
+      'icon-color': '#fc421b',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
