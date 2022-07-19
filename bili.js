ad91c385f605596f95fba4f6dc64c1ae31d7db38
Daniel Kipps <77824387+DanielKipps@users.noreply.github.com>
2022-07-19 19:03:57 +0800

    Create bili.js

---
+++ bili.js
@@ -0,0 +1,25 @@
+let url = "https://api.bilibili.com/x/web-interface/zone"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "IP Check",
+    content: `IP：${ip}\nOperator：${isp}\nArea：${emoji}${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
